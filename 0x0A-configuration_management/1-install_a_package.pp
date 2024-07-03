b95be66d7e95bfe8617f9f34f88b15756c102ac6
ralzaini <rehabalzaini@gmail.com>
2024-07-03 02:41:35 -0700

    aaw

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,17 @@
+# using puppet install flask and pip3
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
